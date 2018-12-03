5b6c11cf094cf9fb62736033fc73540fc5b208b0
filipelauar <44736884+filipelauar@users.noreply.github.com>
2018-12-03 10:56:39 -0200

    Update pessoa.h
    Inclusão do verificador de data de nascimento.

--- pessoa.h
+++ pessoa.h
@@ -11,6 +11,8 @@ class Pessoa
     public:
         Pessoa(string _nome, string _endereco, string _dataDeNascimento, bool _isPessoaFisica, bool _isDoador,double _cpfCnpj);
         //virtual ~pessoa();
+    
+        string verificarData(string _data);
 
         void setNome(string _nome);
         void setEndereco(string _endereco);
